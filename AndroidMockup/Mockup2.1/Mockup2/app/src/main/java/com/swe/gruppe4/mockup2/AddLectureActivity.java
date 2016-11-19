4a09cd2e1fbf5de10101bf8d261080b952f4592c
Merlin Dienst <merlin.dienst@gmail.com>
2016-11-19 14:35:32 +0100

    Date und Timepickerdialoge

--- AndroidMockup/Mockup2.1/Mockup2/app/src/main/java/com/swe/gruppe4/mockup2/AddLectureActivity.java
+++ AndroidMockup/Mockup2.1/Mockup2/app/src/main/java/com/swe/gruppe4/mockup2/AddLectureActivity.java
@@ -1,8 +1,12 @@
 package com.swe.gruppe4.mockup2;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -13,13 +17,25 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import static com.swe.gruppe4.mockup2.R.id.time;
 
-public class AddLectureActivity extends AppCompatActivity {
-    public long setTime;
+public class AddLectureActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText fromDateEtxt;
+    private EditText fromTimeEtxt;
+    private EditText toTimeEtxt;
+
+    private DatePickerDialog fromDatePickerDialog;
+
+    private TimePickerDialog fromTimePickerDialog;
+    private TimePickerDialog toTimePickerDialog;
+
+    private SimpleDateFormat dateFormatter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,32 +56,68 @@ public class AddLectureActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Raum ist zu der ausgewählten Zeit schon blockiert", Toast.LENGTH_LONG).show();
             }
         });
-        EditText dateTimeStart = (EditText) findViewById(R.id.editText3);
-        dateTimeStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final View dialogView = View.inflate(getApplicationContext(), R.layout.date_time_picker, null);
-                final AlertDialog alertDialog = new AlertDialog.Builder(getApplicationContext()).create();
 
-                dialogView.findViewById(R.id.date_time_set).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
+        dateFormatter = new SimpleDateFormat("EEE dd.MM.yyyy", Locale.GERMAN);
 
-                        DatePicker datePicker = (DatePicker) dialogView.findViewById(R.id.date_picker);
-                        TimePicker timePicker = (TimePicker) dialogView.findViewById(R.id.time_picker);
+        findViewsById();
 
-                        Calendar calendar = new GregorianCalendar(datePicker.getYear(),
-                                datePicker.getMonth(),
-                                datePicker.getDayOfMonth(),
-                                timePicker.getCurrentHour(),
-                                timePicker.getCurrentMinute());
+        setDateTimeField();
 
-                        setTime = calendar.getTimeInMillis();
-                        alertDialog.dismiss();
-                    }});
-                alertDialog.setView(dialogView);
-                alertDialog.show();
+    }
+
+    private void findViewsById() {
+        fromDateEtxt = (EditText) findViewById(R.id.DateEditText);
+        fromDateEtxt.setInputType(InputType.TYPE_NULL);
+        fromDateEtxt.requestFocus();
+
+        toTimeEtxt = (EditText) findViewById(R.id.toTime);
+        fromTimeEtxt = (EditText) findViewById(R.id.fromTime);
+
+    }
+
+    private void setDateTimeField() {
+        fromDateEtxt.setOnClickListener(this);
+
+        Calendar newCalendar = Calendar.getInstance();
+        fromDatePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(year, monthOfYear, dayOfMonth);
+                fromDateEtxt.setText(dateFormatter.format(newDate.getTime()));
             }
-        });
+
+        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
+
+        fromTimeEtxt.setOnClickListener(this);
+        fromTimePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(2016,8,11,hourOfDay,minute);
+                fromTimeEtxt.setText(hourOfDay+":"+minute);
+            }
+        },newCalendar.get(Calendar.HOUR_OF_DAY),newCalendar.get(Calendar.MINUTE),true);
+
+        toTimeEtxt.setOnClickListener(this);
+        toTimePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute){
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(2016,8,11,hourOfDay,minute);
+                toTimeEtxt.setText(hourOfDay+":"+minute);
+            }
+        },newCalendar.get(Calendar.HOUR_OF_DAY),newCalendar.get(Calendar.MINUTE),true);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == fromDateEtxt) {
+            fromDatePickerDialog.show();
+        } else if(view == fromTimeEtxt) {
+            fromTimePickerDialog.show();
+        } else if(view == toTimeEtxt) {
+            toTimePickerDialog.show();
+        }
     }
 }
