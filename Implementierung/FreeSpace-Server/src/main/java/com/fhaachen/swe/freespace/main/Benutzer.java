87a6b8cfefe90f7c6223fe99f0a6ce3bee384a95
Thomas Gorgels <thomas.gorgels@googlemail.com>
2016-11-29 13:23:04 +0100

    TG:

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Benutzer.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Benutzer.java
@@ -12,16 +12,18 @@ import java.util.List;
 @Table("Benutzer")
 public class Benutzer extends Model{
 
-
+    static int test = 1234;
     public static boolean test(){
         //Überptüfen ob token und id in DB stehen;
         Base.open("org.sqlite.JDBC", "jdbc:sqlite:./freespace.db", "root", "p@ssw0rd");
         List<Benutzer> b = Benutzer.where("id = ? and token=?", "4711", "abc123");
         System.out.println(b.get(0).toJson(true));
+        Base.close();
         return true;
     }
 
     public static void main(String[] args) {
+
     }
 
 }
