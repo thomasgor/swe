fa2f9eacabd53450fdf80eb9a2b3bdd5d92f2daf
Floix1991 <patrick_wueller@gmx.de>
2016-11-30 16:40:29 +0100

    test

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Datenbank.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Datenbank.java
@@ -7,7 +7,7 @@ import org.javalite.activejdbc.Model;
  * Created by thomasgorgels on 30.11.16.
  */
 public class Datenbank extends Model {
-    public static void connect1(){
+    public static void connect(){
         Base.open("org.sqlite.JDBC", "jdbc:sqlite:./freespace.db", "root", "p@ssw0rd");
     }
 
