b6fd584a8f9dd01f8c94823468ebcfe0f02f02ce
Floix1991 <patrick_wueller@gmx.de>
2016-12-06 15:25:32 +0100

    commit

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Benutzer.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Benutzer.java
@@ -4,22 +4,49 @@ import org.javalite.activejdbc.Base;
 import org.javalite.activejdbc.Model;
 import org.javalite.activejdbc.annotations.Table;
 
+import javax.xml.ws.WebServiceException;
 import java.util.List;
 
 /**
  * Created by thomas on 27.11.2016.
  */
 @Table("Benutzer")
-public class Benutzer extends Model{
+public class Benutzer extends Datenbank{
 
-    static int test = 1234;
-    public static boolean test(){
-        //Überptüfen ob token und id in DB stehen;
+    public static void getDataFromGoogle(int idToken){
 
-        List<Benutzer> b = Benutzer.where("id = ? and token=?", "4711", "abc123");
-        System.out.println(b.get(0).toJson(true));
-        Base.close();
-        return true;
+
+    }
+
+    public static boolean istProfessor(String id){
+        connect();
+        Long count = Benutzer.count("id=? AND istProfessor=1", id);
+        disconnect();
+
+        return(count != null && count >= 1);
+    }
+
+    public static String getRolle(String id){
+        connect();
+        Benutzer b = Benutzer.findById(id);
+        String rolle = null;
+
+        if((Integer) b.get("istProfessor") == 1){
+            rolle = "professor";
+        }else if((Integer) b.get("istProfessor") == 0){
+            rolle = "user";
+        }
+
+        disconnect();
+
+        return rolle;
+    }
+
+    public static boolean istBenutzer(String id){
+        connect();
+        Long count = Benutzer.count("id=?", id);
+        disconnect();
+        return(count != null && count > 0);
     }
 
     public static void main(String[] args) {
