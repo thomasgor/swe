7f413115edc0ba1065692b6fab2feb350a3536be
Simon <simon.rossiter.catley@gmail.com>
2016-12-07 15:52:21 +0100

    Freundschaft Update

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/ressources/FreundschaftREST.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/ressources/FreundschaftREST.java
@@ -1,9 +1,14 @@
 package com.fhaachen.swe.freespace.ressources;
 
+import com.fhaachen.swe.freespace.main.Freundschaft;
+import org.javalite.activejdbc.LazyList;
+
 import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * Created by thomas on 27.11.2016.
@@ -14,8 +19,10 @@ public class FreundschaftREST {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getFreundschaftsliste(){
-        return Response.status(Response.Status.NOT_IMPLEMENTED).entity("Hier ensteht die Freundschaftsliste").build();
+    public Response getFreundschaften(@Context SecurityContext context){
+        String benutzerID = context.getUserPrincipal().getName();
+        String response = Freundschaft.getFreundschaften(benutzerID);
+        return Response.ok(response,MediaType.APPLICATION_JSON).build();
     }
 
     @POST
