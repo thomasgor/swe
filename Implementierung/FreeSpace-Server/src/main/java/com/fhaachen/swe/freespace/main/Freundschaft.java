885c2df68e80f4213625a1e30c587eaf7ad6cf0e
Simon <simon.rossiter.catley@gmail.com>
2016-12-07 15:53:22 +0100

    Update Freundschaft Logik

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Freundschaft.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Freundschaft.java
@@ -1,7 +1,10 @@
 package com.fhaachen.swe.freespace.main;
 
-import org.javalite.activejdbc.Model;
+import com.fhaachen.swe.freespace.JsonHelper;
+import org.javalite.activejdbc.LazyList;
 import org.javalite.activejdbc.annotations.Table;
+
+import java.util.Map;
 
 /**
  * Created by thomas on 27.11.2016.
@@ -9,5 +12,32 @@ import org.javalite.activejdbc.annotations.Table;
 
 @Table("Freundschaft")
 
-public class Freundschaft extends Model {
+public class Freundschaft extends Datenbank{
+
+    private static String includeBenutzer(String json) {
+        Map[] map = JsonHelper.toMaps(json);
+        System.out.println(map[0].toString());
+        Benutzer b = Benutzer.findById(map[0].get("benutzer"));
+        if(b != null) {
+            String json12 = b.toJson(true);
+            Map map2 = JsonHelper.toMap(json12);
+            map[0].put("benutzer", map2);
+        }
+        System.out.println(map[0].toString());
+        return "";
+    }
+
+    public static String getFreundschaften(String benutzerID) {
+        connect();
+        String antwort = "[]";
+        LazyList freunde = Freundschaft.find("benutzer = ? OR freund = ?", benutzerID, benutzerID);
+
+        if(freunde != null) {
+            antwort = freunde.toJson(true);
+        }
+        includeBenutzer(antwort);
+        disconnect();
+        return antwort;
+    }
+
 }
