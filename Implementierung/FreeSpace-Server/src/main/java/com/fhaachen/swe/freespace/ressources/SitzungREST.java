3568a026cfd4b631a8396fe76fe60c99ceb3e6e7
Simon <simon.rossiter.catley@gmail.com>
2016-12-19 19:38:10 +0100

    noch mehr JavaDoc

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/ressources/SitzungREST.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/ressources/SitzungREST.java
@@ -11,11 +11,26 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
 /**
- * Created by thomas on 27.11.2016.
+ * Die Klasse SitzungREST ist die Schnittstelle von HTTP-Request und Server-Logik. Es werden die HTTP-Methoden GET,
+ * POST, PUT und DELETE als REST-Service realisiert, mit dem Pfad http://-Server Domain Namespace-/sitzung
+ *
+ * @author Simon Catley
+ * @version 1.1
  */
 @Path( value = "/sitzung")
 @RolesAllowed({"user", "professor"})
 public class SitzungREST {
+
+
+/**
+ * Diese Methode realisiert den HTTP-GET-Request. Sie erhaelt als Übergabeparameter einen SecurityContext context
+ * aus welcher die ID des über Basic Authentication angemeldeten Benutzers entnommen werden kann.
+ * Liefert ein Response-Objekt mit HTTP-Statuscode und einem Json-String mit der Sitzung des angemeldeten
+ * Benutzers an den Aufrufer zurück.
+ *
+ * @param context SecurityContext des angemeldeten Benutzers
+ * @return Response-Objekt mit HTTP-Statuscode und Json-String mit der Sitzung des Aufrufers
+ */
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -23,7 +38,16 @@ public class SitzungREST {
         String benutzerID = context.getUserPrincipal().getName();
         return Sitzung.getSitzungById(benutzerID);
     }
-
+    /**
+     * Die Realisierung des POST-Request. Erstellt einen neuen Sitzungseintrag in der Datenbank für den angemeldeten
+     * Benutzer, dessen ID über den SecurityContext context ermittelt wird. Des weiteren erhält diese Methode einen String json
+     * im Json-Format, welcher die RaumID des Raumes, indem die Sitzung erstellt werden soll, enthält.
+     * Liefert ein Response-Objekt mit HTTP-Statuscode und einem Json-String mit erstelltem Sitzungseintrag an den Aufrufer zurück.
+     *
+     * @param json String im Json-Format mit RaumId des Raumes der Sitzung
+     * @param context SecurityContext des angemeldeten Benutzers
+     * @return Response-Objekt mit HTTP-Statuscode und Json-String mit erstelltem Sitzungseintrag
+     */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
@@ -31,7 +55,16 @@ public class SitzungREST {
         String benutzerID = context.getUserPrincipal().getName();
         return Sitzung.postSitzung(json, benutzerID);
     }
-
+    /**
+     * Die Realisierung des DELETE-Request. Löscht den Sitzungseintrag des angemeldeten Benutzers aus der Datenbank,
+     * falls die im Übergabeparameter id enthaltene id gleich der des angemeldeten Benutzers ist.
+     * Liefert ein Response-Objekt mit HTTP-Statuscode und einem Json-String mit gelöschtem Sitzungsseintrag an den
+     * Aufrufer zurück.
+     *
+     * @param id ID des Freundes
+     * @param context SecurityContext des angemeldeten Benutzers
+     * @return Response-Objekt mit HTTP-Statuscode und Json-String mit gelöschtem Freundschaftseintrag
+     */
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     @Path(value="/{param}")
@@ -43,6 +76,17 @@ public class SitzungREST {
 
         return Sitzung.deleteSitzung(benutzerID);
     }
+
+    /**
+     * Die Realisierung des PUT-Request. Setzt die Endzeit einer Sitzung auf den Ausgangswert zurück.
+     * Die gesuchte Sitzung wird über die im Überabeparameter id enthaltene ID ermittelt. Die übermittelte ID und die des
+     * angemeldeten Benutzers müssen hierbei gleich sein. Liefert ein Response-Objekt mit
+     * HTTP-Statuscode und einem Json-String mit geaendertem Sitzungseintrag an den Aufrufer zurück.
+     *
+     * @param id ID des Sitzungsinhabers
+     * @param context SecurityContext des angemeldeten Benutzers
+     * @return Response-Objekt mit HTTP-Statuscode und Json-String mit geaendertem Sitzungseintrag
+     */
 
     @PUT
     @Produces(MediaType.APPLICATION_JSON)
--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/ressources/WegREST.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/ressources/WegREST.java
@@ -10,14 +10,27 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 /**
- * Created by Simon on 09.12.2016.
+ * Die Klasse WegREST ist die Schnittstelle von HTTP-Request und Server-Logik. Es wird die HTTP-Methoden GET
+ * als REST-Service realisiert, mit dem Pfad http://-Server Domain Namespace-/weg
+ *
+ * @author Simon Catley
+ * @version 1.0
  */
 
 @Path( value = "/weg")
 @RolesAllowed({"user", "professor"})
 public class WegREST {
 
-        @GET
+    /**
+     * Diese Methode realisiert den HTTP-GET-Request. Sie erhaelt als Übergabeparameter zwei Strings, die die ID's des
+     * Startraumes(startID)und des Zielraumes(zielID) enthalten.
+     * Liefert ein Response-Objekt mit HTTP-Statuscode und einem Json-String mit dem kürzesten weg von start nach ziel
+     * als geordnete Liste an den Aufrufer zurück.
+     *
+     * @return Response-Objekt mit HTTP-Statuscode und Json-String mit dem kürzesten weg von start nach ziel als geordnete Liste
+     */
+
+    @GET
         @Path(value="/{param1}/{param2}")
         @Produces(MediaType.APPLICATION_JSON)
         public Response getWeg(@PathParam(value="param1") String startID ,@PathParam(value="param2") String zielID){
