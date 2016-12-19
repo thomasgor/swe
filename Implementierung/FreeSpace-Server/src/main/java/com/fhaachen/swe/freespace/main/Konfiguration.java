a745ac12e2110f9591885047bef4de49e9329985
Simon <simon.rossiter.catley@gmail.com>
2016-12-19 08:59:42 +0100

    Jsoup-Dependency und fileToString

--- Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Konfiguration.java
+++ Implementierung/FreeSpace-Server/src/main/java/com/fhaachen/swe/freespace/main/Konfiguration.java
@@ -2,6 +2,13 @@ package com.fhaachen.swe.freespace.main;
 
 import org.javalite.activejdbc.annotations.IdName;
 import org.javalite.activejdbc.annotations.Table;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by thomasgorgels on 19.12.16.
@@ -23,5 +30,38 @@ public class Konfiguration extends Datenbank{
         int intervall = Integer.parseInt(Konfiguration.findById("sitzungsintervall").get("value").toString());
         disconnect();
         return intervall;
+    }
+
+    private static String fileToString(String pathname) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(pathname));
+        String         line = null;
+        StringBuilder  stringBuilder = new StringBuilder();
+        String         ls = System.getProperty("line.separator");
+
+        try {
+            while((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+
+            return stringBuilder.toString();
+        } finally {
+            reader.close();
+        }
+    }
+
+
+    private static String putValuesInHTML() {
+        File input = new File("/admin/einstellungen.html");
+        Document doc;
+        return "";
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(fileToString("admin/login.html"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
