17b1d887df54af4e09e29959ff9ec0021cff48b3
Matthias <matthias.maus@yahoo.de>
2017-01-03 20:18:42 +0100

    Objektklasse AktuellerNutzer angegelgt

---
+++ Implementierung/Android App/app/src/main/java/com/swe/gruppe4/freespace/Objektklassen/AktuellerBenutzer.java
@@ -0,0 +1,25 @@
+package com.swe.gruppe4.freespace.Objektklassen;
+
+import com.swe.gruppe4.freespace.Objektklassen.*;
+
+/**
+ *
+ */
+
+public class AktuellerBenutzer {
+    private Benutzer benutzer;
+
+    public AktuellerBenutzer(Benutzer benutzer) {
+        this.benutzer = benutzer;
+    }
+
+    public Benutzer getAktuellerBenutzer() {
+
+        //ToDo: Testdaten gegen valide Daten austauschen
+        return new Benutzer(1,"abc@def.com","Pan","Peter","https://pbs.twimg.com/profile_images/775210778/peter_400x400.JPG", "",1, 0, 0);
+        //return this.benutzer;
+    }
+
+
+
+}
